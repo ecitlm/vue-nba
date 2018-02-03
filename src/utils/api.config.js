7d09b8212bd117205df232cdf7b8d6e3c2e5f9b8
ecitlm <ecitlm@163.com>
2018-02-03 13:55:06 +0800

    add

--- src/utils/api.config.js
+++ src/utils/api.config.js
@@ -9,39 +9,39 @@ import qs from 'qs'
 import md5 from 'md5'
 //axios.defaults.timeout = 5000;
 //axios.defaults.baseURL = 'https://wxapp.it919.cn/?service=';
-const DOMAIN="https://wxapp.it919.cn/?service=";
+const DOMAIN = "https://wxapp.it919.cn/?service=";
 const appkey = '6fc18957ce391f84a7ce34ce13cd99c4';
 
 //POST传参序列化
 axios.interceptors.request.use((config) => {
-    if (config.method === 'post') {
-        config.data = qs.stringify(config.data);
-    }
-    return config;
+  if (config.method === 'post') {
+    config.data = qs.stringify(config.data);
+  }
+  return config;
 }, (error) => {
-    console.log("错误的传参")
-    return Promise.reject(error);
+  console.log("错误的传参")
+  return Promise.reject(error);
 });
 
 
 //获取sign
 var getSign = (params) => {
-  var  params=params || {}
-    for (var key in params) {
-        if (!params[key]) {
-            delete params[key];
-        }
+  var params = params || {}
+  for (var key in params) {
+    if (!params[key]) {
+      delete params[key];
     }
-    params.appkey = appkey;
-    var keyArr = Object.keys(params).sort();
-    var newObj = {};
-    var Kstr = '';
-    for (var i in keyArr) {
-        newObj[keyArr[i]] = params[keyArr[i]];
-        Kstr += params[keyArr[i]];
-    }
-    delete params['appkey'];
-    return md5(Kstr);
+  }
+  params.appkey = appkey;
+  var keyArr = Object.keys(params).sort();
+  var newObj = {};
+  var Kstr = '';
+  for (var i in keyArr) {
+    newObj[keyArr[i]] = params[keyArr[i]];
+    Kstr += params[keyArr[i]];
+  }
+  delete params['appkey'];
+  return md5(Kstr);
 };
 
 /**
@@ -52,44 +52,45 @@ var getSign = (params) => {
  * @returns
  */
 export default {
-    //fetchPost  请求方式
-    fetchPost: function(url, params) {
-      var  params=params || {}
-        params.timestamp = new Date().valueOf(); //将时间戳加入请求参数data里面
-        params.sign = "";
-        params.sign = getSign(params); //将签名加入参数里面
-        return new Promise((resolve, reject) => {
-            axios.post(DOMAIN+url, params)
-                .then(response => {
-                    resolve(response.data);
-                }, err => {
-                    reject(err);
-                })
-                .catch((error) => {
-                    reject(error)
-                })
+  //fetchPost  请求方式
+  fetchPost: function (url, params) {
+    var params = params || {}
+    params.timestamp = new Date().valueOf(); //将时间戳加入请求参数data里面
+    params.sign = "";
+    params.sign = getSign(params); //将签名加入参数里面
+    return new Promise((resolve, reject) => {
+      axios.post(DOMAIN + url, params)
+        .then(response => {
+          resolve(response.data);
+        }, err => {
+          reject(err);
         })
-    },
-
-    //GET 请求方式
-    fetchGet: function(url, params) {
-        params.timestamp = new Date().valueOf();
-        params.sign = "";
-        params.sign = getSign(params || {});
-        return new Promise((resolve, reject) => {
-            axios.get(DOMAIN+url, {
-                    params: params
-                })
-                .then(response => {
-                    resolve(response.data);
-                }, err => {
-                    reject(err);
-                })
-                .catch((error) => {
-                    reject(error)
-                });
+        .catch((error) => {
+          reject(error)
         })
+    })
+  },
 
-    }
+  //GET 请求方式
+  fetchGet: function (url, params) {
+    var params = params || {}
+    params.timestamp = new Date().valueOf();
+    params.sign = "";
+    params.sign = getSign(params || {});
+    return new Promise((resolve, reject) => {
+      axios.get(DOMAIN + url, {
+        params: params
+      })
+        .then(response => {
+          resolve(response.data);
+        }, err => {
+          reject(err);
+        })
+        .catch((error) => {
+          reject(error)
+        });
+    })
+
+  }
 }
 
